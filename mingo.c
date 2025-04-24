f1b4de6c9d919e330ec5fa15e5d7293e26b3f93f
Mingo <kicou79@hotmail.fr>
2025-04-24 13:50:15 +0200

    runned norminette on mingo.c

--- mingo.c
+++ mingo.c
@@ -1,56 +1,81 @@
 #include "mingo.h"
-void ft_readblespace(int count)
+
+void	ft_readblespace(int count)
 {
-    printf("\n exo %d \n", count);
+	printf("\n exo %d \n", count);
 }
-int main(void)
+
+void	ft_c00(int count)
 {
-     int count = 0;
-    // Exemples d'appels de fonctions
-    ft_readblespace(count);
-    count++;
-    ft_putchar('A');
-    ft_readblespace(count++);
-    ft_print_alphabet();
-    ft_readblespace(count++);
-    ft_print_reverse_alphabet();
-    ft_readblespace(count++);
-    ft_print_numbers();
-    ft_readblespace(count++);
-    ft_is_negative(-1);
-    ft_readblespace(count++);
-    ft_print_comb();
-    ft_readblespace(count++);
-    // ft_print_comb2();
-    ft_readblespace(count++);
-    ft_putnbr(42253);
-    ft_readblespace(count++);
-    ft_print_combn(5);
-    ft_readblespace(count++);
-    int nbr = 0;
-    printf("nbr = %d\n", nbr);
-    ft_ft(&nbr);
-    printf("nbr = %d\n", nbr);
-    ft_readblespace(count++);
-    //ft_ultimate_ft(nbr);
-    ft_readblespace(count++);
-    int a = 5, b = 10;
-    ft_swap(&a, &b);
-    ft_readblespace(count++);
-    int div, mod;
-    ft_div_mod(10, 3, &div, &mod);
-    ft_readblespace(count++);
-    ft_ultimate_div_mod(&a, &b);
-    ft_readblespace(count++);
-    ft_putstr("Hello");
-    ft_readblespace(count++);
-    ft_strlen("Hello");
-    ft_readblespace(count++);
-     int tab[] = {1, 2, 3, 4, 5};
-    ft_rev_int_tab(tab, 5);
-    ft_readblespace(count++);
-     
-   
-   
-    return 0;
+	ft_putchar('A');
+	ft_readblespace(count++);
+	ft_print_alphabet();
+	ft_readblespace(count++);
+	ft_print_reverse_alphabet();
+	ft_readblespace(count++);
+	ft_print_numbers();
+	ft_readblespace(count++);
+	ft_is_negative(-1);
+	ft_readblespace(count++);
+	ft_print_comb();
+	ft_readblespace(count++);
+	ft_print_comb2();
+	ft_readblespace(count++);
+	ft_putnbr(42253);
+	ft_readblespace(count++);
+	ft_print_combn(5);
+	ft_readblespace(count++);
+}
+
+void	ft_c01_01(int count, int nbr)
+{
+	int	a;
+	int	b;
+	int	div;
+	int	mod;
+
+	a = 5;
+	b = 10;
+	printf("nbr = %d\n", nbr);
+	ft_ft(&nbr);
+	printf("nbr = %d\n", nbr);
+	ft_readblespace(count++);
+	/*ft_ultimate_ft(nbr);*/
+	ft_readblespace(count++);
+	ft_swap(&a, &b);
+	ft_readblespace(count++);
+	ft_div_mod(10, 3, &div, &mod);
+	ft_readblespace(count++);
+	ft_ultimate_div_mod(&a, &b);
+	ft_readblespace(count++);
+	ft_putstr("Hello");
+	ft_readblespace(count++);
+	ft_strlen("Hello");
+	ft_readblespace(count++);
+}
+
+void	ft_c01_02(int count)
+{
+	int	tab[5];
+
+	tab[0] = 1;
+	tab[1] = 2;
+	tab[2] = 3;
+	tab[3] = 4;
+	tab[4] = 5;
+	ft_rev_int_tab(tab, 5);
+	ft_readblespace(count++);
+}
+
+int	main(void)
+{
+	int	count;
+	int	nbr;
+
+	nbr = 0;
+	count = 0;
+	ft_c00(count);
+	ft_c01_01(count, nbr);
+	ft_c01_02(count);
+	return (0);
 }
